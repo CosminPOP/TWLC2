651e35d428db26b6d6dc05397ca20182bc8630dd
Cosmin POP <mtx.rake@gmail.com>
2020-06-19 23:57:33 +0300

    added re-execute function with delay - GetItemInfo hackfix fade in/out animation for itemframes onupdate on 30fps new backdrops movable frames

--- NeedFrame.lua
+++ NeedFrame.lua
@@ -4,7 +4,7 @@ local NeedFrameComms = CreateFrame("Frame")
 NeedFrameComms:RegisterEvent("CHAT_MSG_ADDON")
 
 LootLCCountdown:Hide()
-LootLCCountdown.timeToNeed = 15
+LootLCCountdown.timeToNeed = 30
 
 LootLCCountdown.T = 1
 LootLCCountdown.C = LootLCCountdown.timeToNeed
@@ -13,39 +13,75 @@ LootLCCountdown.width = 300 --countdown line width
 
 local LootLCNeedFrames = CreateFrame("Frame")
 LootLCNeedFrames.itemFrames = {}
+LootLCNeedFrames.execs = 0
 
+local fadeInAnimationFrame = CreateFrame("Frame")
+fadeInAnimationFrame:Hide()
+fadeInAnimationFrame.ids = {}
+
+local fadeOutAnimationFrame = CreateFrame("Frame")
+fadeOutAnimationFrame:Hide()
+fadeOutAnimationFrame.ids = {}
+
+local delayAddItem = CreateFrame("Frame")
+delayAddItem:Hide()
+delayAddItem.data = {}
+
+delayAddItem:SetScript("OnShow", function()
+    this.startTime = GetTime();
+end)
+delayAddItem:SetScript("OnUpdate", function()
+    local plus = 1
+    local gt = GetTime() * 1000 --22.123 -> 22123
+    local st = (this.startTime + plus) * 1000 -- (22.123 + 0.1) * 1000 =  22.223 * 1000 = 22223
+    if gt >= st then
+
+        local atLeastOne = false
+        for id, data in next, delayAddItem.data do
+            if delayAddItem.data[id] then
+                atLeastOne = true
+                twprint('delay  add item on update')
+                LootLCNeedFrames.addItem(data)
+                delayAddItem.data[id] = nil
+            end
+        end
+
+        if not atLeastOne then
+            delayAddItem:Hide()
+        end
+    end
+end)
 
 LootLCCountdown:SetScript("OnShow", function()
-    this.startTime = math.floor(GetTime());
+    this.startTime = GetTime();
 end)
 
 LootLCCountdown:SetScript("OnUpdate", function()
-    if (math.floor(GetTime()) == math.floor(this.startTime) + 1) then
-        if (LootLCCountdown.T ~= LootLCCountdown.timeToNeed + 1) then
-            --tick
-            getglobal('LootLCNeedFrameWindowTimeLeftCountdownFrameTimeLeft'):SetText(LootLCCountdown.C - LootLCCountdown.T + 1 .. "s")
-            getglobal('LootLCNeedFrameWindowTimeLeftCountdownFrame'):SetWidth(math.floor(LootLCCountdown.C - LootLCCountdown.T + 1) * LootLCCountdown.width / LootLCCountdown.timeToNeed)
-            if (LootLCCountdown.C - LootLCCountdown.T + 1 >= 20) then
-                getglobal('LootLCNeedFrameWindowTimeLeftCountdownFrame'):SetBackdropColor(0, 0.88, 0.06, 1);
-            elseif (LootLCCountdown.C - LootLCCountdown.T + 1 > 10 and LootLCCountdown.C - LootLCCountdown.T + 1 < 20) then
-                getglobal('LootLCNeedFrameWindowTimeLeftCountdownFrame'):SetBackdropColor(1, 0.5, 0.02, 1);
-            else
-                getglobal('LootLCNeedFrameWindowTimeLeftCountdownFrame'):SetBackdropColor(1, 0, 0, 1);
+    local plus = 0.03
+    local gt = GetTime() * 1000 --22.123 -> 22123
+    local st = (this.startTime + plus) * 1000 -- (22.123 + 0.1) * 1000 =  22.223 * 1000 = 22223
+    if gt >= st then
+        if (LootLCCountdown.T ~= LootLCCountdown.timeToNeed + plus) then
+
+            for index in next, LootLCNeedFrames.itemFrames do
+                if (math.floor(LootLCCountdown.C - LootLCCountdown.T + plus) < 0) then
+                    getglobal('LCNeedFrame' .. index .. 'TimeLeftBarText'):SetText("CLOSED")
+                else
+                    getglobal('LCNeedFrame' .. index .. 'TimeLeftBarText'):SetText(math.ceil(LootLCCountdown.C - LootLCCountdown.T + plus) .. "s")
+                end
+
+                getglobal('LCNeedFrame' .. index .. 'TimeLeftBar'):SetWidth((LootLCCountdown.C - LootLCCountdown.T + plus) * 190 / LootLCCountdown.timeToNeed)
             end
         end
         LootLCCountdown:Hide()
-        if (LootLCCountdown.T < LootLCCountdown.C + 1) then
+        if (LootLCCountdown.T < LootLCCountdown.C + plus) then
             --still tick
-            LootLCCountdown.T = LootLCCountdown.T + 1
+            LootLCCountdown.T = LootLCCountdown.T + plus
             LootLCCountdown:Show()
-        elseif (LootLCCountdown.T == LootLCCountdown.timeToNeed + 1) then
-
-            --end
-            getglobal('LootLCNeedFrameWindowTimeLeftCountdownFrameTimeLeft'):SetText("CLOSED")
-            --            getglobal('LootLCNeedFrameWindow'):Hide()
+        elseif (LootLCCountdown.T > LootLCCountdown.timeToNeed + plus) then
 
             -- hide frames and send auto pass
-            for index, f in next, LootLCNeedFrames.itemFrames do
+            for index in next, LootLCNeedFrames.itemFrames do
                 if (LootLCNeedFrames.itemFrames[index]:IsVisible()) then
                     PlayerNeedItemButton_OnClick(index, 'autopass')
                 end
@@ -54,19 +90,6 @@ LootLCCountdown:SetScript("OnUpdate", function()
 
             LootLCCountdown:Hide()
             LootLCCountdown.T = 1
-
-
-            --            local j = 0
-            --            for n, v in next, LootLC.votes do
-            --                j = j + 1
-            --            end
-
-            --            if (j == 0) then
-            --                lcprint("Nobody linked")
-            --            else
-            --                LootLC:AddPlayers() -- ML/RL View
-            --                getglobal("LootLCWindow"):SetHeight(170 + j * 40)
-            --            end
 
         else
             --
@@ -77,38 +100,69 @@ LootLCCountdown:SetScript("OnUpdate", function()
 end)
 
 function LootLCNeedFrames.addItem(data)
+    twdebug('data : ' .. data)
     local item = string.split(data, "=")
+
+    LootLCNeedFrames.execs = LootLCNeedFrames.execs + 1
+    twprint('additem exec : ' .. LootLCNeedFrames.execs)
 
     local index = tonumber(item[2])
     local texture = item[3]
     local name = item[4]
     local link = item[5]
 
-    SendAddonMessage("TWLCNF", "wait=" .. index .. "=0=0", "RAID")
+    local _, _, itemLink = string.find(link, "(item:%d+:%d+:%d+:%d+)");
 
-    GetItemInfo(link) --cache ???
+    GameTooltip:SetHyperlink(itemLink)
+    GameTooltip:Hide()
+
+    local name, _, quality, _, _, _, _, _, tex = GetItemInfo(itemLink)
+
+    if (not name or not quality) then
+        delayAddItem.data[index] = data
+        delayAddItem:Show()
+        return false
+    end
+
+    LootLCNeedFrames.execs = 0
+
+    SendAddonMessage("TWLCNF", "wait=" .. index .. "=0=0", "RAID")
 
     if (not LootLCNeedFrames.itemFrames[index]) then
         LootLCNeedFrames.itemFrames[index] = CreateFrame("Frame", "LCNeedFrame" .. index, getglobal("LootLCNeedFrameWindow"), "NeedFrameItemTemplate")
     end
 
+    local backdrop = {
+        bgFile = "Interface\\Addons\\TWLC2\\images\\need\\need_" .. quality,
+        tile = false,
+    };
+
+    getglobal('LCNeedFrame' .. index .. 'BgImage'):SetBackdrop(backdrop)
+
     LootLCNeedFrames.itemFrames[index]:Show()
-    LootLCNeedFrames.itemFrames[index]:SetPoint("TOP", getglobal("LootLCNeedFrameWindow"), "TOP", 0, 0 - (55 * index))
+    LootLCNeedFrames.itemFrames[index]:SetAlpha(0)
+    LootLCNeedFrames.itemFrames[index]:SetPoint("TOP", getglobal("LootLCNeedFrameWindow"), "TOP", 0, (80 * index))
     LootLCNeedFrames.itemFrames[index].link = link
+
+    --todo minimap dropdown, option to show loot anchor, to move
 
     getglobal('LCNeedFrame' .. index .. 'ItemIcon'):SetNormalTexture(texture);
     getglobal('LCNeedFrame' .. index .. 'ItemIcon'):SetPushedTexture(texture);
-    getglobal('LCNeedFrame' .. index .. 'ItemName'):SetText(link);
+    getglobal('LCNeedFrame' .. index .. 'ItemIconItemName'):SetText(link);
 
     getglobal('LCNeedFrame' .. index .. 'BISButton'):SetID(index);
     getglobal('LCNeedFrame' .. index .. 'MSUpgradeButton'):SetID(index);
     getglobal('LCNeedFrame' .. index .. 'OSButton'):SetID(index);
     getglobal('LCNeedFrame' .. index .. 'PassButton'):SetID(index);
 
+    local r, g, b = GetItemQualityColor(quality)
+
+    getglobal('LCNeedFrame' .. index .. 'TimeLeftBar'):SetBackdropColor(r, g, b, .76)
+
     addOnEnterTooltipNeedFrame(getglobal('LCNeedFrame' .. index .. 'ItemIcon'), link)
 
-
-    CalcMainWindowHeight()
+    fadeInFrame(index)
+    --    CalcMainWindowHeight()
     --    getglobal('LootLCNeedFrameWindow'):Show()
 end
 
@@ -149,11 +203,81 @@ function PlayerNeedItemButton_OnClick(id, need)
     end
 
     SendAddonMessage("TWLCNF", need .. "=" .. id .. "=" .. myItem1 .. "=" .. myItem2, "RAID")
-    LootLCNeedFrames.itemFrames[id]:Hide()
 
-    groupNeedFrames()
-    CalcMainWindowHeight()
+    fadeOutFrame(id)
+
+    --    groupNeedFrames()
+    --    CalcMainWindowHeight()
 end
+
+function fadeInFrame(id)
+    fadeInAnimationFrame.ids[id] = true
+    fadeInAnimationFrame:Show()
+end
+
+function fadeOutFrame(id)
+    fadeOutAnimationFrame.ids[id] = true
+    fadeOutAnimationFrame:Show()
+end
+
+fadeInAnimationFrame:SetScript("OnShow", function()
+    this.startTime = GetTime()
+end)
+
+fadeOutAnimationFrame:SetScript("OnShow", function()
+    this.startTime = GetTime()
+end)
+
+fadeInAnimationFrame:SetScript("OnUpdate", function()
+    if ((GetTime()) >= (this.startTime) + 0.03) then
+
+        this.startTime = GetTime()
+
+        local atLeastOne = false
+        for id in next, fadeInAnimationFrame.ids do
+            if fadeInAnimationFrame.ids[id] then
+                atLeastOne = true
+                local frame = getglobal("LCNeedFrame" .. id)
+                if (frame:GetAlpha() < 1) then
+                    frame:SetAlpha(frame:GetAlpha() + 0.2)
+                else
+                    fadeInAnimationFrame.ids[id] = false
+                    fadeInAnimationFrame.ids[id] = nil
+
+                end
+                return
+            end
+        end
+        if (not atLeastOne) then
+            fadeInAnimationFrame:Hide()
+        end
+    end
+end)
+
+fadeOutAnimationFrame:SetScript("OnUpdate", function()
+    if ((GetTime()) >= (this.startTime) + 0.03) then
+
+        this.startTime = GetTime()
+
+        local atLeastOne = false
+        for id in next, fadeOutAnimationFrame.ids do
+            if fadeOutAnimationFrame.ids[id] then
+                atLeastOne = true
+                local frame = getglobal("LCNeedFrame" .. id)
+                if (frame:GetAlpha() > 0) then
+                    frame:SetAlpha(frame:GetAlpha() - 0.15)
+                else
+                    fadeOutAnimationFrame.ids[id] = false
+                    fadeOutAnimationFrame.ids[id] = nil
+                    frame:Hide()
+                end
+            end
+        end
+        if (not atLeastOne) then
+            fadeOutAnimationFrame:Hide()
+        end
+    end
+end)
 
 function groupNeedFrames()
     local visibleFrames = 0;
@@ -188,7 +312,7 @@ function LootLCNeedFrame.ResetVars()
     for index, frame in next, LootLCNeedFrames.itemFrames do
         LootLCNeedFrames.itemFrames[index]:Hide()
     end
-    CalcMainWindowHeight()
+    --    CalcMainWindowHeight()
     getglobal('LootLCNeedFrameWindow'):Hide()
     LootLCCountdown.T = 1
     LootLCCountdown.width = 300
@@ -222,6 +346,15 @@ NeedFrameComms:SetScript("OnEvent", function()
     end
 end)
 
+function NFIT_DragStart()
+    getglobal('LootLCNeedFrameWindow'):StartMoving();
+    getglobal('LootLCNeedFrameWindow').isMoving = true;
+end
+
+function NFIT_DragEnd()
+    getglobal('LootLCNeedFrameWindow'):StopMovingOrSizing();
+    getglobal('LootLCNeedFrameWindow').isMoving = false;
+end
 
 -- utils
 
